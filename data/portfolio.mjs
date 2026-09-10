73ac5b093a468140f678d91821a1afc02ca6893e
Sanjoy Kumar Sarkar <imsanjoy562@gmail.com>
2026-09-10 13:57:06 +0600

    Add whatsapp button

--- data/portfolio.mjs
+++ data/portfolio.mjs
@@ -4,7 +4,7 @@ export const person = {
   "shortName": "Sanjoy Sarkar",
   "title": "IT & Digital Systems Professional",
   "email": "imsanjoy562@gmail.com",
-  "whatsapp": "",
+  "whatsapp": "+8801724353766",
   "linkedin": "https://www.linkedin.com/in/sksarkar/",
   "github": "https://github.com/sanjoyksarkar",
   "siteUrl": "",
